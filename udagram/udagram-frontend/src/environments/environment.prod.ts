aade546ed0976303d939b3715c2e4547eb733803
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-11-09 21:28:29 +0200

    fix url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://contactsapi-env-1.eba-xwbxa3ac.us-east-1.elasticbeanstalk.com/',
+  apiHost: 'http://contactsapi-env-1.eba-xwbxa3ac.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
