eb9bce8098f5d88cd0a1a112ecae327903722ce5
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-11-10 19:20:34 +0200

    fix

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,5 +1,5 @@
-echo 'n' | eb init \"$APP_NAME\" --region \"$REGION\"
-eb use \"$ENV\"
+echo 'n' | eb init "$APP_NAME" --region $REGION
+eb use $ENV
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY      
 eb setenv AWS_BUCKET=$AWS_BUCKET AWS_PROFILE=$AWS_PROFILE AWS_REGION=$AWS_REGION URL=$URL      
 eb setenv JWT_SECRET=$JWT_SECRET      
