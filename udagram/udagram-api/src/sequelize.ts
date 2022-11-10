816688564b006bdd3651e87b3f56d77e5b9eb5d6
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-11-10 13:43:59 +0200

    test

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+console.log(
+  config.username,
+  config.database,
+  config.password,
+  config.host,
+)
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
